2187ffdbca722aefd75484cf416b055aea6144a1
王超 <wangchao@aldwx.com>
2018-05-24 21:33:06 +0800

    更改caseclass为伴生对象 提供一些静态方法 写的还算精简，没有注释，看看什么时候会忘

--- src/test/TestBitMap.scala
+++ src/test/TestBitMap.scala
@@ -1,15 +1,15 @@
 
 object TestBitMap {
   def main(args: Array[String]): Unit = {
-    val bitMap= BitMap(10)
+    val bitMap = new BitMap(4)
     bitMap.setBit(0)
     bitMap.setBit(1)
     bitMap.setBit(2)
-    bitMap.setBit(2)
-    bitMap.setBit(32)
-    bitMap.setBit(3)
-    bitMap.setBit(319)
-
-bitMap.show
+    bitMap.setBit(115)
+    BitMap.setBit(bitMap.getMap, 3)
+    BitMap.show(bitMap.getMap)
+    bitMap.show
+    val result = BitMap.count(bitMap.getMap)
+    println(result)
   }
 }
